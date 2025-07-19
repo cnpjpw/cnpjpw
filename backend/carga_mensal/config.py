3030d2b21d5fd70132fa73b94dcceeaa140268b2
João Cavalcante <cavalcante.joao@protonmail.ch>
2025-07-19 02:44:41 +0200

    trocando chaves dos tipos dos indices

--- backend/carga_mensal/config.py
+++ backend/carga_mensal/config.py
@@ -1,31 +1,45 @@
 TIPOS_INDICES = {
-    'empresas': {
+    'Empresas': {
         'date': [],
         'numeric': [4],
         'bool': [],
         'array': [],
         'id': [0]
     },
-    'estabelecimentos': {
+    'Estabelecimentos': {
         'date': [6, 10, 29],
         'numeric': [],
         'bool': [],
         'array': [12],
         'id': [0, 1, 2]
     },
-    'dados_simples': {
+    'Simples': {
         'date': [2, 3, 5, 6],
         'numeric': [],
         'bool': [1, 4],
         'array': [],
         'id': [0]
     },
-    'socios': {
+    'Socios': {
         'date': [5],
         'numeric': [],
         'bool': [],
         'array': [],
         'id': []
     },
+
+    'Outros': {
+        'date': [],
+        'numeric': [],
+        'bool': [],
+        'array': [],
+        'id': []
+    }
 }
+
+
+
+AUXILIARES = ['Cnaes', 'Motivos', 'Municipios', 'Naturezas', 'Paises', 'Qualificacoes', 'Simples']
+PRINCIPAIS = ['Empresas', 'Estabelecimentos', 'Socios']
+NOMES_ARQUIVOS = AUXILIARES + [f'{p}{i}' for p in PRINCIPAIS for i in range(10)]
 
