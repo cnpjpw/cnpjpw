a31357356fe565656cfeb588f9709b65bc634060
João Cavalcante <cavalcante.joao@protonmail.ch>
2025-08-04 17:43:01 -0300

    movendo logica leitura data

--- backend/etl/main.py
+++ backend/etl/main.py
@@ -10,22 +10,8 @@ from config import *
 import psycopg
 from tqdm import tqdm
 import logging
-
-
-def ler_data_json(path):
-    with open(path, "r") as f:
-        data_pasta = json.load(f)
-
-    return (data_pasta['mes'], data_pasta['ano'])
-
-
-def acrescentar_mes_json(path, mes, ano):
-    with open(path, "w") as f:
-        data_dic = {
-            'mes': (mes % 12) + 1,
-            'ano': ano + (mes // 12)
-        }
-        json.dump(data_dic, f)
+from utils import ler_data_json
+from utils import acrescentar_mes_json
 
 
 def main():
---
+++ backend/etl/utils.py
@@ -0,0 +1,15 @@
+def ler_data_json(path):
+    with open(path, "r") as f:
+        data_pasta = json.load(f)
+
+    return (data_pasta['mes'], data_pasta['ano'])
+
+
+def acrescentar_mes_json(path, mes, ano):
+    with open(path, "w") as f:
+        data_dic = {
+            'mes': (mes % 12) + 1,
+            'ano': ano + (mes // 12)
+        }
+        json.dump(data_dic, f)
+
