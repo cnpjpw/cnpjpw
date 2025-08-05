82ed68254bdd118eafed067d106e34bc2c922f36
Jao42 <joaovictormedeiros41@gmail.com>
2025-08-05 10:42:30 -0300

    removendo indice e config nao usada

--- backend/bd-queries/pos_criacao.sql
+++ backend/bd-queries/pos_criacao.sql
@@ -5,8 +5,7 @@ CREATE INDEX motivo_situacao_cadastral_idx ON estabelecimentos(motivo_situacao_c
 CREATE INDEX idx_socios_cnpj_base ON socios (cnpj_base);
 CREATE INDEX razao_social_idx ON empresas (nome_empresarial);
 CREATE INDEX capital_social_idx ON empresas (capital_social);
-CREATE INDEX razao_social_segmentos_texto_idx ON empresas (nome_empresarial text_pattern_ops);
-CREATE INDEX razao_social_segmentos_texto2_idx ON empresas (nome_empresarial text_pattern_ops, cnpj_base);
+CREATE INDEX razao_social_segmentos_texto_idx ON empresas (nome_empresarial text_pattern_ops, cnpj_base);
 CREATE INDEX natureza_juridica_idx ON empresas (natureza_juridica);
 CREATE INDEX capital_cnpj_empresas_idx ON empresas (capital_social, cnpj_base);
 CREATE INDEX estabelecimentos_municipio_idx ON estabelecimentos (municipio);
@@ -20,9 +19,7 @@ CREATE INDEX idx_est_full_query ON estabelecimentos (cnae_fiscal_principal, situ
 CREATE INDEX estabelecimentos_uf_idx ON estabelecimentos (uf);
 CREATE INDEX uf_data_abertura_cnpj_estabelecimentos_idx ON estabelecimentos (uf, data_inicio_atividade, cnpj_base, cnpj_ordem, cnpj_dv);
 
-ALTER TABLE estabelecimentos ALTER COLUMN data_inicio_atividade SET STATISTICS 200;
 VACUUM ANALYZE empresas;
 VACUUM ANALYZE estabelecimentos;
 VACUUM ANALYZE socios;
 
--- \copy table FROM 'filename' (ENCODING 'latin-1', DELIMITER ';', FORMAT csv, HEADER false)
