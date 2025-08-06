14a6d8894e9d4f71dee1c9d846ed99013ea81036
João Cavalcante <cavalcante.joao@protonmail.ch>
2025-08-06 17:12:52 -0300

    modularizando mais carga mensal

--- backend/etl/main.py
+++ backend/etl/main.py
@@ -6,7 +6,6 @@ from parsing import parse_csv_tabela
 from load import *
 import pathlib
 import json
-from config import *
 import psycopg
 from tqdm import tqdm
 import logging
@@ -14,80 +13,22 @@ from utils import ler_data_json
 from utils import acrescentar_mes_json
 
 
-def polling_carga_mensal():
-    PATH_SCRIPT = pathlib.Path(__file__).parent
-
-    logging.basicConfig(
-        filename=PATH_SCRIPT / 'rotina_mensal.log',
-        level=logging.INFO,
-        format="%(asctime)s [%(levelname)s] %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-    )
-    logger = logging.getLogger(__name__)
-
-    logger.info('Carregando Variáveis de Ambiente e Configurações')
-    load_dotenv()
-    BD_NOME = os.environ['BD_NOME']
-    BD_USUARIO = os.environ['BD_USUARIO']
-    PATH_RAIZ = pathlib.Path(os.environ['PATH_CNPJ_DADOS_RAIZ'])
-
-    path_json_data = PATH_SCRIPT / "data_pasta.json"
-    mes, ano = ler_data_json(path_json_data)
-    path_dados = PATH_RAIZ / f'{str(mes).zfill(2)}-{ano}'
-
-    logger.info('Criando diretórios(se não existirem)')
-    (path_dados / 'zip').mkdir(parents=True, exist_ok=True)
-    (path_dados / 'tmp').mkdir(parents=True, exist_ok=True)
-    (path_dados / 'csv').mkdir(parents=True, exist_ok=True)
-
-    logger.info('Iniciando Download dos Zips da Receita')
-    download_cnpj_zips(ano, mes, path_dados / "zip")
-    logger.info('Iniciando Extração dos Zips da Receita')
-    extrair_zips(path_dados / "zip", path_dados / "tmp", nao_numerados=NAO_NUMERADOS, numerados=NUMERADOS)
-
-    logger.info("Iniciando Tratamento Inicial dos CSV's da Receita")
-    for nome in (NAO_NUMERADOS + NUMERADOS):
+def tratar_dados_abertos(nomes_csv, tipos_indices, path_dados):
+    for nome in nomes_csv:
         nome_arquivo = nome + '.csv'
         path_entrada = path_dados / 'tmp'
         path_saida = path_dados / 'csv'
         parse_csv_tabela(
-            indices_tipo=TIPOS_INDICES.get(nome, TIPOS_INDICES['Outros']),
+            indices_tipo=tipos_indices.get(nome, tipos_indices['Outros']),
             nome_arquivo=nome_arquivo,
             path_entrada=path_entrada,
             path_saida=path_saida,
         )
         (path_entrada / nome_arquivo).unlink()
-    logger.info('Iniciando Rotinas de Carga em BD')
-    with psycopg.connect(dbname=BD_NOME, user=BD_USUARIO) as conn:
-        carregar_arquivos_bd(AUXILIARES, PRINCIPAIS, path_dados, ARQ_TABELA_DIC, conn, True)
-    logger.info('Modificando Mês de Download dos Dados')
-    acrescentar_mes_json(path_json_data, mes, ano)
-    logger.info('Carga Mensal Concluida')
-
-
-def polling_carga_diaria():
-    logging.basicConfig(
-        filename=PATH_SCRIPT / 'rotina_diaria.log',
-        level=logging.INFO,
-        format="%(asctime)s [%(levelname)s] %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-    )
-    logger = logging.getLogger(__name__)
-
-    logger.info('Carregando Variáveis de Ambiente e Configurações')
-    load_dotenv()
-    BD_NOME = os.environ['BD_NOME']
-    BD_USUARIO = os.environ['BD_USUARIO']
-
-    path_dados = ''
-
-    logger.info('Iniciando Rotinas de Carga em BD')
-    with psycopg.connect(dbname=BD_NOME, user=BD_USUARIO) as conn:
-        carregar_arquivos_bd([], PRINCIPAIS, path_dados, ARQ_TABELA_DIC, conn, True)
-    logger.info('Carga Mensal Concluida')
 
 
 def carregar_arquivos_bd(auxiliares, principais, path_dados, arq_tabela_dic, conn, faz_update):
+        from config import ARQ_TABELA_DIC
         logger.info('Carregando Dados Auxiliares nas Tabelas de Staging(direto para o staging2)')
         for nome in tqdm(auxiliares):
             nome_tabela = f'{ARQ_TABELA_DIC[nome]}_staging2'
@@ -122,7 +63,58 @@ def carregar_arquivos_bd(auxiliares, principais, path_dados, arq_tabela_dic, con
             )
 
 
+def polling_carga_mensal(bd_nome, bd_usuario, path_raiz, path_script, logger):
+    logger.info('Carregando Variáveis e Configurações')
+    from config import NAO_NUMERADOS, NUMERADOS, AUXILIARES, PRINCIPAIS, TIPOS_INDICES, ARQ_TABELA_DIC
+
+    path_json_data = path_script / "data_pasta.json"
+    mes, ano = ler_data_json(path_json_data)
+    path_dados = path_raiz / f'{str(mes).zfill(2)}-{ano}'
+
+    logger.info('Criando diretórios(se não existirem)')
+    for subdir in ['zip', 'tmp', 'csv']:
+        (path_dados / subdir).mkdir(parents=True, exist_ok=True)
+
+    logger.info('Iniciando Download dos Zips da Receita')
+    download_cnpj_zips(ano, mes, path_dados / "zip")
+
+    logger.info('Iniciando Extração dos Zips da Receita')
+    extrair_zips(path_dados / "zip", path_dados / "tmp", nao_numerados=NAO_NUMERADOS, numerados=NUMERADOS)
+
+    logger.info("Iniciando Tratamento Inicial dos CSV's da Receita")
+    tratar_dados_abertos(NAO_NUMERADOS + NUMERADOS, TIPOS_INDICES, path_dados)
+
+    logger.info('Iniciando Rotinas de Carga em BD')
+    with psycopg.connect(dbname=bd_nome, user=bd_usuario) as conn:
+        carregar_arquivos_bd(AUXILIARES, PRINCIPAIS, path_dados, ARQ_TABELA_DIC, conn, True)
+
+    logger.info('Modificando Mês de Download dos Dados')
+    acrescentar_mes_json(path_json_data, mes, ano)
+    logger.info('Carga Mensal Concluida')
+
+
+def polling_carga_diaria(bd_nome, bd_usuario, path_raiz, path_script, logging):
+    logger.info('Carregando Variáveis e Configurações')
+    from config import PRINCIPAIS, ARQ_TABELA_DIC
+    path_dados = path_raiz / 'csv'
+    logger.info('Iniciando Rotinas de Carga em BD')
+    with psycopg.connect(dbname=bd_nome, user=bd_usuario) as conn:
+        carregar_arquivos_bd([], PRINCIPAIS, path_dados, ARQ_TABELA_DIC, conn, True)
+    logger.info('Carga Mensal Concluida')
+
 
 if __name__ == '__main__':
-    polling_carga_mensal()
+    load_dotenv()
+    BD_NOME = os.environ['BD_NOME']
+    BD_USUARIO = os.environ['BD_USUARIO']
+    PATH_RAIZ = pathlib.Path(os.environ['PATH_CNPJ_DADOS_RAIZ'])
+    PATH_SCRIPT = pathlib.Path(__file__).parent
+    logging.basicConfig(
+        filename=PATH_SCRIPT / 'rotina_mensal.log',
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    logger = logging.getLogger(__name__)
+    polling_carga_mensal(BD_NOME, BD_USUARIO, PATH_RAIZ, PATH_SCRIPT, logger)
 
