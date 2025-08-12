5f2e657b9f0922fb5e73754e374dbcd55bf84d2d
Jao42 <joaovictormedeiros41@gmail.com>
2025-08-12 11:22:16 -0300

    adicionando logica polling carga diaria

---
+++ backend/etl/polling_carga_diaria.py
@@ -0,0 +1,94 @@
+from dotenv import load_dotenv
+import os
+import pathlib
+import logging
+import requests
+from requests.exceptions import ReadTimeout
+from cnpjpw_scrapers.config import HEADERS
+from cnpjpw_scrapers import gerar_novos_cnpjs
+from cnpjpw_scrapers import get_cnpj_info
+from cnpjpw_scrapers import parse_scrape_data
+from tqdm import tqdm
+from load import pegar_ultimo_cnpj_inserido
+from main import carregar_arquivos_bd
+import csv
+import psycopg
+from time import sleep
+
+def polling_carga_diaria(bd_nome, bd_usuario, path_raiz, path_script, limite_maximo, logger):
+    from config import PRINCIPAIS, ARQ_TABELA_DIC
+    s = requests.session()
+    s.headers = HEADERS
+
+    with psycopg.connect(dbname=bd_nome, user=bd_usuario) as conn:
+        logger.info('Obtendo CNPJ mais Recente')
+        primeiro_cnpj = pegar_ultimo_cnpj_inserido(conn)
+    logger.info('Baixando Novas Paginas')
+    download_paginas(primeiro_cnpj, limite_maximo, s)
+    logger.info('Fazendo Parsing das Paginas')
+    nomes = os.listdir(path_raiz / 'tmp')
+    tratar_paginas(nomes, path_raiz)
+
+    logger.info('Iniciando Rotinas de Carga em BD')
+    with psycopg.connect(dbname=bd_nome, user=bd_usuario) as conn:
+        carregar_arquivos_bd([], PRINCIPAIS, path_raiz, ARQ_TABELA_DIC, conn, False, logger)
+    logger.info('Carga Diária Concluida')
+
+
+def download_paginas(primeiro_cnpj, quantidade_max, s):
+    cnpjs = gerar_novos_cnpjs(primeiro_cnpj, quantidade_max)
+    count = 0
+    for cnpj in tqdm(cnpjs):
+        if count == 5:
+            break
+        try:
+            html = get_cnpj_info(cnpj, s)
+        except ReadTimeout:
+            continue
+        if html is None:
+            count += 1
+            continue
+        with open(PATH_RAIZ / 'tmp' / f'{cnpj}.html', 'w') as f:
+            f.write(html)
+        count = 0
+        sleep(0.25)
+
+
+def tratar_paginas(nomes, path_raiz):
+    from config import PRINCIPAIS, ARQ_TABELA_DIC
+    for pagina in tqdm(nomes):
+        with open(path_raiz / 'tmp' / pagina) as f:
+            html = f.read()
+        dados = parse_scrape_data(html)
+        for nome in PRINCIPAIS:
+            tabela = ARQ_TABELA_DIC[nome]
+            tabela_json = dados[tabela]
+            with open(path_raiz / 'csv' / f'{nome}.csv', 'a') as csvfile:
+                if tabela == 'socios':
+                    for socio in tabela_json:
+                        writer = csv.writer(csvfile, delimiter=';')
+                        writer.writerow(socio.values())
+                    continue
+                writer = csv.writer(csvfile, delimiter=';')
+                writer.writerow(tabela_json.values())
+        (path_raiz / 'tmp' / pagina).unlink()
+
+
+if __name__ == '__main__':
+    PATH_SCRIPT = pathlib.Path(__file__).parent
+    logging.basicConfig(
+        filename=PATH_SCRIPT / 'rotina_diaria.log',
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    logger = logging.getLogger(__name__)
+    logger.info('Carregando Variáveis e Configurações')
+    load_dotenv()
+    BD_NOME = os.environ['BD_NOME']
+    BD_USUARIO = os.environ['BD_USUARIO']
+    PATH_RAIZ = pathlib.Path(os.environ['PATH_CNPJ_DADOS_DIARIOS'])
+    limite_maximo = 2000
+
+    polling_carga_diaria(BD_NOME, BD_USUARIO, PATH_RAIZ, PATH_SCRIPT, limite_maximo, logger)
+
--- backend/etl/utils.py
+++ backend/etl/utils.py
@@ -1,4 +1,51 @@
 import json
+import psycopg
+from cnpjpw_scrapers import gerar_novos_cnpjs
+
+
+def pegar_ultimo_cnpj_inserido(conn):
+    with conn.cursor() as cursor:
+        query_data = (
+            "SELECT data_inicio_atividade FROM estabelecimentos ORDER BY data_inicio_atividade DESC LIMIT 1"
+        )
+
+        data = cursor.execute(query_data).fetchone()[0]
+        query_cnpj = (
+                "SELECT cnpj_base AS cnpj " +
+                "FROM estabelecimentos where data_inicio_atividade = (%s)::date " +
+                "ORDER BY cnpj_base DESC LIMIT 1"
+                )
+        cnpj = cursor.execute(query_cnpj, (data, )).fetchone()[0]
+    return cnpj
+
+
+def pegar_primeiro_cnpj_dia(conn):
+    with conn.cursor() as cursor:
+        query_data = (
+            "SELECT data_inicio_atividade FROM estabelecimentos ORDER BY data_inicio_atividade DESC LIMIT 1"
+        )
+        data = cursor.execute(query_data).fetchone()[0]
+        query_primeiro = (
+            "SELECT cnpj_base FROM estabelecimentos WHERE data_inicio_atividade = (%s)::date ORDER BY cnpj_base ASC LIMIT 1"
+        )
+        primeiro = cursor.execute(query_primeiro, (data, )).fetchone()[0]
+    return primeiro
+
+
+def pegar_buracos_dia(conn):
+    with conn.cursor() as cursor:
+        primeiro = pegar_primeiro_cnpj_dia(conn)
+        ultimo = pegar_ultimo_cnpj_inserido(conn)
+        query_cnpjs = (
+            "select cnpj_base || cnpj_ordem || cnpj_dv from estabelecimentos where cnpj_base > (%s)::bpchar AND cnpj_base < (%s)::bpchar ORDER BY cnpj_base"
+        )
+        res = cursor.execute(query_cnpjs, (primeiro, ultimo)).fetchall()
+    cnpjs = [k[0] for k in res]
+    total = int(cnpjs[-1][:8]) - int(cnpjs[0][:8]) + 1
+    gerados = gerar_novos_cnpjs(cnpjs[0], total)
+    dif_cnpjs = list(set(gerados) - set(cnpjs))
+    return dif_cnpjs
+
 
 def ler_data_json(path):
     with open(path, "r") as f:
